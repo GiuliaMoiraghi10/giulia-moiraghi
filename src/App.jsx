30ef2147088619e7fc6892a806578b8217b6aa2b
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2025-09-10 12:27:36 +0200

    Creo repo, installo react, tailwind, font e primi elementi home

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import giulia from './assets/giulia.png'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-xl'>Ciao, sono Giulia!</h1>
+        <img src={giulia} alt="" />
+      </div>
+    </>
+  )
+}
+
+export default App
