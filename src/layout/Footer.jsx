a132bcc7da9830d05a90a906a8d05884fb8d38f2
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2025-09-18 09:56:38 +0200

    Header css e Footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -2,8 +2,9 @@ import logo from '../assets/logo.jpg'
 
 export default function Footer() {
     return (
-        <footer>
-            <img src={logo} alt="" />
+        <footer className="fixed bottom-0 left-0 w-full flex justify-center items-center">
+            {/* <img src={logo} alt="Logo" /> */}
+            Footer
         </footer>
     )
 }
