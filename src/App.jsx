2cf08d6508762c4c3cfa3a05b2eeccaaa66eaf0e
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2025-09-18 19:04:35 +0200

    Modify package.json

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <>
-      <BrowserRouter basename="/giulia-moiraghi/">
+      <BrowserRouter>
         <Routes>
           <Route element={<DefaultLayout />}>
             <Route path='/' element={<HomePage />} ></Route>
